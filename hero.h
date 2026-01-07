59e7702bed22dc21ebffdd46a5c0561ebdfaebef
Kacperiix <kacperkor3@gmail.com>
2026-01-07 19:46:38 +0100

    Struktura projektu i definicja typów danych
    - Utworzono podział na moduły: main.c, hero.c oraz hero.h.
    - Zdefiniowano strukturę Bohater i typ wyliczeniowy (enum) dla statusów.
    - Zaimplementowano obsługę argumentów wywołania (ścieżka do pliku).
    - Stworzono szkielet menu głównego.

---
+++ hero.h
@@ -0,0 +1,26 @@
+#ifndef HERO_H
+#define HERO_H
+
+typedef enum {
+    AKTYWNY,
+    NA_MISJI,    
+    RANNY,
+    ZAGINIONY,
+    ZAWIESZONY
+} StatusBohatera;
+
+typedef struct {
+    char imie[101];     
+    char rasa[50];
+    char klasa[50];
+    int poziom;          
+    int reputacja;    
+    StatusBohatera status;
+} Bohater;
+
+typedef struct ElementListy {
+    Bohater dane;                
+    struct ElementListy* nastepny; 
+} ElementListy;
+
+#endif
---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "hero.h"
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("BLAD: Nie podano nazwy pliku z baza danych!\n");
+        printf("Uzycie: %s <nazwa_pliku.txt>\n", argv[0]);
+        return 1;
+    }
+
+    char* nazwaPliku = argv[1];
+    printf("Uruchomiono system Gildii. Plik danych: %s\n", nazwaPliku);
+
+    int wybor;
+    do {
+        printf("\n=== REJESTR BOHATEROW GILDII ===\n");
+        printf("1. Dodaj bohatera\n");
+        printf("2. Wyswietl wszystkich\n");
+        printf("3. Usun bohatera\n");
+        printf("4. Wyjscie\n");
+        printf("Wybierz opcje: ");
+        
+        if (scanf("%d", &wybor) != 1) {
+            printf("To nie jest liczba!\n");
+            while(getchar() != '\n'); 
+            wybor = 0; 
+        }
+
+        switch (wybor) {
+            case 1:
+                printf("--- Tu bedzie dodawanie ---\n");
+                break;
+            case 2:
+                printf("--- Tu bedzie wyswietlanie ---\n");
+                break;
+            case 3:
+                printf("--- Tu bedzie usuwanie ---\n");
+                break;
+            case 4:
+                printf("Konczenie pracy...\n");
+                break;
+            default:
+                printf("Nieznana opcja. Sprobuj ponownie.\n");
+        }
+    } while (wybor != 4);
+
+    return 0;
+}
